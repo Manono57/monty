9275b12987f8151c3586ff4bdd2dfcc8b773a24d
Manono <manonospaces@gmail.com>
2023-08-27 23:46:04 +0300

    task 4

--- main.c
+++ main.c
@@ -1,52 +1,49 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include "monty.h"
 
-#define BUFFER_SIZE 1024
-
-vars var;
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - Start LIFO, FILO program
- * @ac: Number of arguments
- * @av: Pointer containing arguments
- * Return: 0 Success, 1 Failed
- */
-int main(int ac, char **av)
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
+
+int main(int argc, char *argv[])
 {
-    char *opcode;
-    char buffer[BUFFER_SIZE];
-    
-    if (ac != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        return (EXIT_FAILURE);
-    }
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-    if (start_vars(&var) != 0)
-        return (EXIT_FAILURE);
-
-    var.file = fopen(av[1], "r");
-    if (!var.file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-        free_all();
-        return (EXIT_FAILURE);
-    }
-
-    while (fgets(buffer, sizeof(buffer), var.file) != NULL)
-    {
-        opcode = strtok(buffer, " \r\t\n");
-        if (opcode != NULL)
-        {
-            if (call_funct(&var, opcode) == EXIT_FAILURE)
-            {
-                free_all();
-                return (EXIT_FAILURE);
-            }
-        }
-        var.line_number++;
-    }
-
-    free_all();
-
-    return (EXIT_SUCCESS);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
